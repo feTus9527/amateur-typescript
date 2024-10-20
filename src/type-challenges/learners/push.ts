4b179c21ecff2380977cf315c81a74e4efb5d036
fetus <snow1013895225@gmail.com>
2024-10-20 17:14:32 +0800

    feat: type statement of Array.push

---
+++ src/type-challenges/learners/push.ts
@@ -0,0 +1,13 @@
+// https://typehero.dev/challenge/push
+
+/// Input START
+type Push<T extends Array<PropertyKey>, U> = [...T, U];
+/// Input END
+
+module Test {
+  type cases = [
+    Expect<Equal<Push<[], 1>, [1]>>,
+    Expect<Equal<Push<[1, 2], "3">, [1, 2, "3"]>>,
+    Expect<Equal<Push<["1", 2, "3"], boolean>, ["1", 2, "3", boolean]>>,
+  ];
+}
