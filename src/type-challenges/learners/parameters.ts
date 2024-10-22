e5cfbb4a3969bf1f11ba938e67b58750a5a7c78d
fetus <snow1013895225@gmail.com>
2024-10-22 10:23:37 +0800

    feat: type statement of parameters

--- src/type-challenges/learners/parameters.ts
+++ src/type-challenges/learners/parameters.ts
@@ -1,7 +1,11 @@
 // https://typehero.dev/challenge/parameters
-// FIXME
+
 /// Input START
-type MyParameters<T extends (...args: any[]) => any> = any;
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...args: infer A
+) => any
+  ? A
+  : never;
 /// Input END
 
 namespace ParametersTest {
