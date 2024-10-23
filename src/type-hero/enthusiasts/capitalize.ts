4e2c1e9c387de1d59a529059464e5c88f6a1c963
fetus <snow1013895225@gmail.com>
2024-10-23 13:07:29 +0800

    feat: type statement of capitalizing a string

--- src/type-hero/enthusiasts/capitalize.ts
+++ src/type-hero/enthusiasts/capitalize.ts
@@ -1,7 +1,39 @@
 // https://typehero.dev/challenge/capitalize
-// FIXME
+
 /// Input START
-type MyCapitalize<S extends string> = any;
+type LowerToUpper = {
+  a: "A";
+  b: "B";
+  c: "C";
+  d: "D";
+  e: "E";
+  f: "F";
+  g: "G";
+  h: "H";
+  i: "I";
+  j: "J";
+  k: "K";
+  l: "L";
+  m: "M";
+  n: "N";
+  o: "O";
+  p: "P";
+  q: "Q";
+  r: "R";
+  s: "S";
+  t: "T";
+  u: "U";
+  v: "V";
+  w: "W";
+  x: "X";
+  y: "Y";
+  z: "Z";
+};
+type MyCapitalize<S extends string> = S extends `${infer L}${infer R}`
+  ? L extends keyof LowerToUpper
+    ? `${LowerToUpper[L]}${R}`
+    : S
+  : S;
 /// Input END
 
 namespace CapitalizeTest {
