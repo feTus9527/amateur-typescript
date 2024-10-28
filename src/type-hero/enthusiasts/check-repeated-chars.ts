30c77128cce0ef329216a6f698df5f5bacb32bca
fetus <snow1013895225@gmail.com>
2024-10-28 14:42:38 +0800

    feat: type statement of checking repeated chars in a string

--- src/type-hero/enthusiasts/check-repeated-chars.ts
+++ src/type-hero/enthusiasts/check-repeated-chars.ts
@@ -1,7 +1,14 @@
 // https://typehero.dev/challenge/checkrepeatedchars
-// FIXME
+
 /// Input START
-type CheckRepeatedChars<T extends string> = any;
+type CheckRepeatedChars<
+  T extends string,
+  A extends any[] = [],
+> = T extends `${infer L}${infer R}`
+  ? L extends A[number]
+    ? true
+    : CheckRepeatedChars<R, [...A, L]>
+  : false;
 /// Input END
 
 namespace CheckRepeatedCharsTest {
